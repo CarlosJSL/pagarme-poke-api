546fa050976a3d3e6c4227089391ba5f064f351f
Carlos josé <carlosjsl95@gmail.com>
2017-10-25 23:40:33 -0300

    Adicionando validação nos campos do sequelize

--- models/pokemon.js
+++ models/pokemon.js
@@ -3,15 +3,24 @@ export default(sequelize, DataType) => {
     name: {
       type: DataType.STRING,
       allowNull: false,
+      isAlpha: {
+        msg: 'O nome só pode conter letras',
+      },
     },
     price: {
       type: DataType.INTEGER,
       allowNull: false,
+      isNumeric:{
+        msg: 'O preço só pode conter números',
+      }
     },
     stock: {
       type: DataType.INTEGER,
       allowNull: true,
       defaultValue: 1,
+      isNumeric:{
+        msg: 'O estoque só pode conter números',
+      }
     },
   })
   return Pokemon
